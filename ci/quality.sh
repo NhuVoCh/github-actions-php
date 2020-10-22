2ceeafbcee5ca28c2d17e9005ab2870260a2ad9e
Nhu-Hoai Robert Vo <nhuhoai.vo@nhuvo.ch>
2020-10-23 00:23:09 +0200

    Allow multiple PHP versions.

--- ci/quality.sh
+++ ci/quality.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:ondrej/php
 sudo apt update
-sudo apt install -y wget php php-tokenizer php-xml
+sudo apt install -y wget "php${1}" "php${1}-tokenizer" "php${1}-xml"
 sudo wget -O /usr/local/bin/phpcs https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
 sudo chmod +x /usr/local/bin/phpcs
 php --version
--- ci/syntax.sh
+++ ci/syntax.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:ondrej/php
 sudo apt update
-sudo apt install -y php
+sudo apt install -y "php${1}"
 php --version
 find src -type f -name "*.php" | xargs -n1 php -l

01cbcc4bed179499bdf7061b847109e6b0ec02d8
Nhu-Hoai Robert Vo <nhuhoai.vo@nhuvo.ch>
2020-10-22 23:51:40 +0200

    Fix PHPCS errors.

--- src/index.php
+++ src/index.php
@@ -1,1 +1,18 @@
 <?php
+
+/**
+ * Main file
+ *
+ * PHP Version 7.4
+ *
+ * @category  Test
+ * @package   GitHubActionsPhpTests
+ * @author    Nhu-Hoai Robert Vo <nhuhoai.vo@nhuvo.ch>
+ * @copyright 2020 Nhu-Hoai Robert Vo
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @version   GIT: 0.1.0
+ * @link      https://github.com/NhuVoCh/github-actions-php/
+ * @since     0.1.0
+ */
+
+namespace NhuVo\GitHubActionsPhpTests;
